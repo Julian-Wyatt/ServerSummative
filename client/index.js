e5e5a2dfebae80f72a61e62f4714b31bc2eba713
Julian Wyatt <Julian_Wyatt@hotmail.com>
2019-03-06 16:12:58 +0000

    copy and pasted recents.json which client adds videos to first 4

--- client/index.js
+++ client/index.js
@@ -1,15 +1,22 @@
+/* global gapi */
+
 function onSignIn(googleUser) {
 	var profile = googleUser.getBasicProfile();
-	console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-	console.log('Name: ' + profile.getName());
+	console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
+	console.log("Name: " + profile.getName());
 	let accntText = document.getElementById("accntName");
 	accntText.textContent = profile.getName();
-	console.log('Image URL: ' + profile.getImageUrl());
-	console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+	console.log("Image URL: " + profile.getImageUrl());
+	console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+
+	// send info to server and handle there
+
 	
 }
 
-function signOut(){
+function signOut(){		//add this with DOM 
+	// same here, ping server, then ping back with sign out 
 	var auth2 = gapi.auth2.getAuthInstance();
 	auth2.signOut().then(function () {
 		console.log("User signed out.");
@@ -36,13 +43,28 @@ document.addEventListener("DOMContentLoaded", function () {
 			let body = await response.text();
 			
 			let recents = JSON.parse(body);
-			console.log(recents);
+			return recents;
+
 		}
 		catch (e){
 			alert(e);
 		}
 	}
 
-	recent();
+	recent().then(function (videoData){
+		console.log(videoData);
+		for (let i=1;i<=4;i++){
+			let frame = document.getElementById("video"+i);
+			frame.src = "https://www.youtube.com/embed/" + videoData["items"][i-1]["id"]["videoId"];
+			let title = document.getElementById("title"+i);
+			title.innerHTML = videoData["items"][i-1]["snippet"]["title"];
+		}
+	});
+
+	let gSignOut = document.getElementById("gSignOut");
+	gSignOut.addEventListener("click",signOut);
+	
+
+	
 
 });             
--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 var express = require("express");
 var app  = express();
+var fs = require("fs");
+var readline = require("readline");
+const {google} = require("googleapis");
+//const {OAuth2Client} = require("google-auth-library");
+
+
+
+
+
+
 
 
 app.use(express.static("client"));
@@ -22,16 +32,164 @@ app.get("/placeholder.png", function(req, res) {
 	res.sendFile(__dirname + "/client/" + "placeholder.png");
 });
 
-let recents = [1,"2","3","4","5"];
+
 app.get("/recent", function(req,res){
 
-	
-	
-	res.send(recents);
+
+	res.send(fs.readFileSync("recents.json"));
 	//res.send(recents);
 	res.end();
 });
 
+
+var SCOPES = ["https://www.googleapis.com/auth/youtube.force-ssl","https://www.googleapis.com/auth/youtube"];
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + "/.credentials/";
+var TOKEN_PATH = TOKEN_DIR + "google-apis-nodejs-quickstart.json";
+
+
+
+// fs.readFile("client_secret.json", function processClientSecrets(err, content) {
+// 	if (err) {
+// 		console.log("Error loading client secret file: " + err);
+// 		return;
+// 	}
+// 	// Authorize a client with the loaded credentials, then call the YouTube API.
+// 	//See full code sample for authorize() function code.
+// 	authorize(JSON.parse(content), {"params": {
+// 		"maxResults": "25",
+// 		"part": "snippet",
+// 		"q": "Official Trailer",
+// 		"type": "video"}}, searchListByKeyword);
+  
+// });
+
+
+function authorize(credentials, requestData, callback) {
+	var clientSecret = credentials.installed.client_secret;
+	var clientId = credentials.installed.client_id;
+	var redirectUrl = credentials.installed.redirect_uris[0];
+	
+	var oauth2Client = new google.auth.OAuth2(clientId, clientSecret, redirectUrl);
+  
+	// Check if we have previously stored a token.
+	fs.readFile(TOKEN_PATH, function(err, token) {
+		if (err) {
+			getNewToken(oauth2Client, requestData, callback);
+		} else {
+			oauth2Client.credentials = JSON.parse(token);
+			callback(oauth2Client, requestData);
+		}
+	});
+}
+
+  
+function getNewToken(oauth2Client, requestData, callback) {
+	var authUrl = oauth2Client.generateAuthUrl({
+		access_type: "offline",
+		prompt: "consent",
+		scope: SCOPES
+	});
+	console.log("Authorize this app by visiting this url: ", authUrl);
+	var rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
+	rl.question("Enter the code from that page here: ", function(code) {
+		rl.close();
+		oauth2Client.getToken(code, function(err, token) {
+			if (err) {
+				console.log("Error while trying to retrieve access token", err);
+				return;
+			}
+			oauth2Client.credentials = token;
+			storeToken(token);
+			callback(oauth2Client, requestData);
+		});
+	});
+}
+
+function storeToken(token) {
+	try {
+		fs.mkdirSync(TOKEN_DIR);
+	} catch (err) {
+		if (err.code != "EEXIST") {
+			throw err;
+		}
+	}
+	fs.writeFileSync(TOKEN_PATH, JSON.stringify(token));
+	console.log("Token stored to " + TOKEN_PATH);
+}
+
+
+function removeEmptyParameters(params) {
+	for (var p in params) {
+		if (!params[p] || params[p] == "undefined") {
+			delete params[p];
+		}
+	}
+	return params;
+}
+
+/*
+function createResource(properties) {
+	var resource = {};
+	var normalizedProps = properties;
+	for (var p in properties) {
+		var value = properties[p];
+		if (p && p.substr(-2, 2) == "[]") {
+			var adjustedName = p.replace("[]", "");
+			if (value) {
+				normalizedProps[adjustedName] = value.split(",");
+			}
+			delete normalizedProps[p];
+		}
+	}
+	for (var p in normalizedProps) {
+	// Leave properties that don't have values out of inserted resource.
+		if (normalizedProps.hasOwnProperty(p) && normalizedProps[p]) {
+			var propArray = p.split(".");
+			var ref = resource;
+			for (var pa = 0; pa < propArray.length; pa++) {
+				var key = propArray[pa];
+				if (pa == propArray.length - 1) {
+					ref[key] = normalizedProps[p];
+				} 
+				else {
+					ref = ref[key] = ref[key] || {};
+				}
+			}
+		}
+	}
+	return resource;
+}
+*/
+
+function searchListByKeyword(auth, requestData) {
+	var service = google.youtube("v3");
+	//var parameters = requestData["params"];
+	var parameters = removeEmptyParameters(requestData["params"]);
+	parameters["auth"] = auth;
+	service.search.list(parameters, function(err, response) {
+		if (err) {
+			console.log("The API returned an error: " + err);
+			return;
+		}
+		
+		//console.log(response);
+		// console.log(response["data"]["items"][0]);
+		// console.log(response["data"]["items"][2]);
+		// console.log(response["data"]["items"][3]);
+		// console.log(response["data"]["items"][6]);
+
+		fs.writeFileSync("recents.json",JSON.stringify(response),function(err){
+			if (err){
+				console.log("error occured when writing");
+			}
+		});
+	});
+	
+}
 
 
 app.listen(8080);
