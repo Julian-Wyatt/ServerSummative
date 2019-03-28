5390c07da4ad46fe74d15ce26c91ad33d2d6f64f
Julian Wyatt <Julian_Wyatt@hotmail.com>
2019-03-28 15:25:17 +0000

    added interval stuff here

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
-const app = require("./app.js");
+const server = require("./app.js");
 
-app.listen(process.env.PORT || 8080);
+setInterval(server.intervalSavingRecents, 1000 * 60 * 45);
+// TEST THE CODE WITH THIS: setInterval(intervalSavingRecents, 1000 * 60);
+// delete line after 60 seconds if testing as the daily quota will be used up quickly
+setInterval(server.intervalSavingChannels, 1000 * 60 * 60 * 6);
+
+server.app.listen(process.env.PORT || 8080);
