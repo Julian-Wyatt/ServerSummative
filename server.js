a0f266597a11bad8c352cb01f96ec15354e792f9
Julian Wyatt <Julian_Wyatt@hotmail.com>
2019-04-07 15:34:21 +0100

    comment

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const server = require("./app.js");
 
 setInterval(server.intervalSavingRecents, 1000 * 60 * 45);
+
+// setInterval(server.intervalSavingRecents, 1000 * 4);
+
 // TEST THE CODE WITH THIS: setInterval(intervalSavingRecents, 1000 * 60);
 // delete line after 60 seconds if testing as the daily quota will be used up quickly
 setInterval(server.intervalSavingChannels, 1000 * 60 * 60 * 6);
